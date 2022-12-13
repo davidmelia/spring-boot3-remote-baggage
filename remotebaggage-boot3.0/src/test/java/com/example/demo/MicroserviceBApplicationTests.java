037d4a07902677637b9bb59e2f1a232ebc42266c
meliad2 <david.melia@ii.co.uk>
2022-12-13 14:15:48 +0000

    better

--- remotebaggage-boot3.0/src/test/java/com/example/demo/MicroserviceBApplicationTests.java
+++ remotebaggage-boot3.0/src/test/java/com/example/demo/MicroserviceBApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class MicroserviceBApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void customerNameShouldBeDave() {
+
+    Map result = WebClient.builder().baseUrl("http://localhost:9081/addresses/1234").defaultHeader("Customer-Name", "Dave").build().get().retrieve().bodyToMono(Map.class).block();
+    Assertions.assertEquals("Dave", result.get("customerName"), "result should contain 'Dave'");
+
+  }
 
 }
+
